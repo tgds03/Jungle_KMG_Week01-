15c32b8730c9e935d56e8c54d1f36a0f73d796af
jiwon1304 <jiwon1304@gmail.com>
2025-03-07 21:29:19 +0900

    USceneComponent init

--- Week01+_KMG/Framework/Core/USceneComponent.cpp
+++ Week01+_KMG/Framework/Core/USceneComponent.cpp
@@ -1,2 +1,142 @@
 #include "stdafx.h"
 #include "USceneComponent.h"
+
+FVector4 USceneComponent::GetRelativeLocation() const
+{
+	return RelativeLocation;
+}
+
+FVector4 USceneComponent::GetRelativeRotation() const
+{
+	return RelatvieRotation;
+}
+
+FVector4 USceneComponent::GetRelativeScale3D() const
+{
+	return RelativeScale3D;
+}
+
+FVector4 USceneComponent::GetComponentVelocity() const
+{
+	return ComponentVelocity;
+}
+
+void USceneComponent::SetRelativeLocation(FVector4 Location)
+{
+	RelativeLocation = Location;
+}
+
+void USceneComponent::SetRelativeRotation(FVector4 Rotation)
+{
+	RelatvieRotation = Rotation;
+}
+
+void USceneComponent::SetRelativeScale3D(FVector4 Scale3D)
+{
+	RelativeScale3D = Scale3D;
+}
+
+void USceneComponent::SetComponentVelocity(FVector4 Velocity)
+{
+	ComponentVelocity = Velocity;
+}
+
+// const랑 pass by reference(&)
+// 데이터를 효율적으로 전달하면서 호출한 함수가 이 오브젝트의 children을 바꾸지 못하게 함
+// 리턴 값을 다른 변수에 옮기고서("=" operator) 사용
+const TArray<USceneComponent*>& USceneComponent::GetAttachChildren() const
+{
+	return AttachChildern;
+}
+
+void USceneComponent::GetChildrenComponents(TArray<USceneComponent*>& Children) const
+{
+	Children.clear();
+	for (auto& child : Children)
+	{
+		TArray<USceneComponent*> childComponents;
+		child->GetChildrenComponents(childComponents);
+		Children.insert(Children.end(), childComponents.begin(), childComponents.end());
+	}
+}
+
+USceneComponent* USceneComponent::GetAttachParent() const
+{
+	return AttachParent;
+}
+
+void USceneComponent::GetParentComponents(TArray<USceneComponent*>& Parents) const
+{
+	Parents.clear();
+
+	// 언리얼 소스코드 /Engine/Source/Runtime/Engine/Classes/Components/SceneComponent.h 참고
+	USceneComponent* ParentIterator = GetAttachParent();
+	while (ParentIterator != nullptr)
+	{
+		Parents.push_back(ParentIterator);
+		ParentIterator = ParentIterator->GetAttachParent();
+	}
+}
+
+// pass by reference로 메모리 절약
+void USceneComponent::SetupAttachment(TArray<USceneComponent*>& Children)
+{
+	AttachChildern.clear();
+
+	for (auto& child : Children)
+	{
+		if (child->AttachToComponent(this))
+		{
+			UE_LOG(L"USceneComponent::SetupAttachment::Children으로 지정하는데 실패했습니다.");
+		}
+	}
+}
+
+//void USceneComponent::AttachChildren(USceneComponent* Child)
+//{
+//	if (Child == this)
+//	{
+//		UE_LOG(L"자기 자신을 Children으로 할 수 없습니다.");
+//		return;
+//	}
+//	TArray<USceneComponent*> parents;
+//	GetParentComponents(parents);
+//	for (auto& parent : parents)
+//	{
+//		if (this == parent->GetAttachParent()) {
+//			UE_LOG(L"Ancestor를 Children으로 할 수 없습니다.");
+//			return;
+//		}
+//	}
+//	AttachChildern.push_back(Child);
+//}
+
+bool USceneComponent::AttachToComponent(USceneComponent* Parent)
+{
+	if (Parent == this)
+	{
+		UE_LOG(L"USceneComponent::AttachToComponent::자기 자신을 Parent로 할 수 없습니다.");
+		return false;
+	}
+
+	// parent로 하려는 컴포넌트의 조상에 *this가 있는지 확인
+	TArray<USceneComponent*> parentsOfParent;
+	GetParentComponents(parentsOfParent);
+	if (parentsOfParent.end() != std::find(parentsOfParent.begin(), parentsOfParent.end(), this))
+	{
+		UE_LOG(L"USceneComponent::AttachToComponent::Descendent를 Parent로 할 수 없습니다.");
+		return false;
+	}
+
+	Parent->AttachChildern.push_back(Parent);
+	return true;
+
+	//TArray<USceneComponent*> children;
+	//GetChildrenComponents(children);
+	//if (children.end() != std::find(children.begin(), children.end(), this))
+	//{
+
+	//}
+	//return true;
+}
+
--- Week01+_KMG/Framework/Core/USceneComponent.h
+++ Week01+_KMG/Framework/Core/USceneComponent.h
@@ -1,11 +1,42 @@
 #pragma once
 #include "Framework/Core/UActorComponent.h"
 class USceneComponent :public UActorComponent {
-public:
+
+// 물리, 변환 관련
+private:
 	FVector4 RelativeLocation;
 	FVector4 RelatvieRotation;
 	FVector4 RelativeScale3D;
+	FVector4 ComponentVelocity;
 
-	FMatrix Transformation();
+public:
+	FVector4 GetRelativeLocation() const;
+	FVector4 GetRelativeRotation() const;
+	FVector4 GetRelativeScale3D() const; 
+	FVector4 GetComponentVelocity() const;
+
+	void SetRelativeLocation(_In_ FVector4 Location);
+	void SetRelativeRotation(_In_ FVector4 Rotation);
+	void SetRelativeScale3D(_In_ FVector4 Scale3D);
+	void SetComponentVelocity(_In_ FVector4 Velocity);
+
+// 상속 관련
+private:
+	USceneComponent* AttachParent = nullptr;
+	TArray<USceneComponent*> AttachChildern;
+
+public:
+	const TArray<USceneComponent*>& GetAttachChildren() const; // 직접적으로 붙은 children만 반환
+	void GetChildrenComponents(_Out_ TArray<USceneComponent*>& Children) const; // 재귀로 아래의 모든 children 반환
+	USceneComponent* GetAttachParent() const;
+	void GetParentComponents(_Out_ TArray<USceneComponent*>& Parents) const; // 재귀로 root까지 반환
+
+	void SetupAttachment(_In_ TArray<USceneComponent*>& Children); // 자식 설정 실패 가능
+
+	// 부모자식 관계를 실제로 정하는 이 함수 뿐임
+	bool AttachToComponent(_In_ USceneComponent* Parent); // 부모 설정 실패했을땐 false
+
+	// **자식 추가하는 함수는 금지; AttachToComponent 사용해야함**
+	//void AttachChildren(_In_ USceneComponent* Child);
+	//void SetAttachParent(_In_ USceneComponent* Parent);
 };
-
