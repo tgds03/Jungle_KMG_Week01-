e3611c5e7860f26fab41b1072c002993d4fa01dd
jiwon1304 <jiwon1304@gmail.com>
2025-03-09 01:24:32 +0900

    plane vertex (0,0,0) 기준으로 변경

--- Week01+_KMG/Framework/Core/UPlaneComponent.cpp
+++ Week01+_KMG/Framework/Core/UPlaneComponent.cpp
@@ -3,17 +3,32 @@
 #include "./Framework/Core/CRenderer.h"
 
 UPlaneComponent::UPlaneComponent() {
-	vertices = {
-		{10.f, -5.f, 10.f,		1.f, 0.f, 0.f, 1.f},
-		{-10.f, -5.f, 10.f,		0.f, 1.f, 0.f, 1.f},
-		{-10.f, -5.f, -10.f,	0.f, 0.f, 1.f, 1.f },
-		{10.f, -5.f, -10.f,		1.f, 1.f, 1.f, 1.f }
+	vertices = 
+	{
+		//   x,   y,   z       r,   g,   b,   a
+		{-1.0f, 0.0f, -1.0f, 1.0f, 0.0f, 0.0f, 1.0f }, // ÁÂÇÏ´Ü (»¡°­)
+		{ 1.0f, 0.0f, -1.0f, 0.0f, 1.0f, 0.0f, 1.0f }, // ¿ìÇÏ´Ü (ÃÊ·Ï)
+		{ 1.0f, 0.0f,  1.0f, 0.0f, 0.0f, 1.0f, 1.0f }, // ¿ì»ó´Ü (ÆÄ¶û)
+		{-1.0f, 0.0f,  1.0f, 1.0f, 1.0f, 1.0f, 1.0f }, // ÁÂ»ó´Ü (Èò»ö)
+	};
+	
+	indices = 
+	{
+		0, 1, 2,  // Ã¹ ¹øÂ° »ï°¢Çü
+		0, 2, 3   // µÎ ¹øÂ° »ï°¢Çü
 	};
 
-	indices = {
-		0, 1, 2,
-		2, 3, 0
-	};
+	//vertices = {
+	//	{10.f, -5.f, 10.f,		1.f, 0.f, 0.f, 1.f},
+	//	{-10.f, -5.f, 10.f,		0.f, 1.f, 0.f, 1.f},
+	//	{-10.f, -5.f, -10.f,	0.f, 0.f, 1.f, 1.f },
+	//	{10.f, -5.f, -10.f,		1.f, 1.f, 1.f, 1.f }
+	//};
+
+	//indices = {
+	//	0, 1, 2,
+	//	2, 3, 0
+	//};
 
 	CGraphics* graphics = CRenderer::Instance()->GetGraphics();
 	_vertexBuffer = new CVertexBuffer<FVertexSimple>(graphics->GetDevice());
