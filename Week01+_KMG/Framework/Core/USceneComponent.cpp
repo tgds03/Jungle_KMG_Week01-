fec94af57f450029f6aafd8b667a8ca5416b276e
jiwon1304 <jiwon1304@gmail.com>
2025-03-08 20:38:02 +0900

    hierarchy 추가

--- Week01+_KMG/Framework/Core/USceneComponent.cpp
+++ Week01+_KMG/Framework/Core/USceneComponent.cpp
@@ -1,11 +1,301 @@
 #include "stdafx.h"
 #include "USceneComponent.h"
 
-FMatrix USceneComponent::Transformation() {
-	FMatrix m = FMatrix::Scale(RelativeScale3D.x, RelativeScale3D.y, RelativeScale3D.z);
-	m = m * FMatrix::RotateX(RelativeRotation.x);
-	m = m * FMatrix::RotateY(RelativeRotation.y);
-	m = m * FMatrix::RotateZ(RelativeRotation.z);
-	m = m * FMatrix::Translate(RelativeLocation.x, RelativeLocation.y, RelativeLocation.z);
-	return m;
+void USceneComponent::Update()
+{
+	UActorComponent::Update();
 }
+
+//void USceneComponent::Render()
+//{
+//	UActorComponent::Render();
+//}
+
+FVector USceneComponent::GetRelativeLocation() const
+{
+	return RelativeLocation;
+}
+
+FVector4 USceneComponent::GetRelativeLocation4() const
+{
+	return FVector4(RelativeLocation, 1);
+}
+
+FVector USceneComponent::GetRelativeRotation() const
+{
+	return RelativeRotation;
+}
+
+FVector USceneComponent::GetRelativeScale3D() const
+{
+	return RelativeScale3D;
+}
+
+FMatrix USceneComponent::GetRelativeTransform() const
+{
+	//FMatrix origin = FMatrix(FVector4::Zero, FVector4::Zero, FVector4::Zero, FVector4(0, 0, 0, 1));
+	FMatrix origin = FMatrix::Identity;
+
+	FMatrix scale = FMatrix::Scale(RelativeScale3D);
+	FMatrix rot = FMatrix::RotateX(RelativeRotation.x) * FMatrix::RotateY(RelativeRotation.y) * FMatrix::RotateZ(RelativeRotation.z);
+	FMatrix trans = FMatrix::Translate(RelativeLocation);
+
+	return origin * scale * rot * trans;
+}
+
+FVector USceneComponent::GetComponentLocation() const
+{
+	if (AttachParent != nullptr)
+	{
+		FVector4 componentVec = (FVector4(GetRelativeLocation(), 1) * AttachParent->GetComponentTransform());
+		return FVector(componentVec.xyz() / componentVec.w);
+	}
+	else
+	{
+		return GetRelativeLocation();
+	}
+}
+
+//FVector USceneComponent::GetComponentRotation() const
+//{
+//	if (AttachParent != nullptr)
+//	{
+//		return FVector((FVector4(GetRelativeRotation(), 0) * AttachParent->GetComponentTransform()).xyz());
+//	}
+//	else
+//	{
+//		return GetRelativeRotation();
+//	}
+//}
+//
+//FVector USceneComponent::GetComponentScale() const
+//{
+//	if (AttachParent != nullptr)
+//	{
+//		return FVector((FVector4(GetRelativeScale3D(), 0) * AttachParent->GetComponentTransform()).xyz());
+//	}
+//	else
+//	{
+//		return GetRelativeScale3D();
+//	}
+//}
+FMatrix USceneComponent::GetComponentTransform() const
+{
+	if (AttachParent != nullptr)
+	{
+		return GetRelativeTransform() * AttachParent->GetComponentTransform();
+	}
+	else
+	{
+		return GetRelativeTransform();
+	}
+}
+
+void USceneComponent::SetRelativeLocation(const FVector NewLocation)
+{
+	RelativeLocation = NewLocation;
+}
+
+void USceneComponent::SetRelativeLocationX(const float NewLocX)
+{
+	RelativeLocation.x = NewLocX;
+}
+
+void USceneComponent::SetRelativeLocationY(const float NewLocY)
+{
+	RelativeLocation.y = NewLocY;
+}
+
+void USceneComponent::SetRelativeLocationZ(const float NewLocZ)
+{
+	RelativeLocation.z = NewLocZ;
+}
+
+void USceneComponent::SetRelativeRotation(const FVector NewRotation)
+{
+	RelativeRotation = NewRotation;
+}
+
+void USceneComponent::SetRelativeRotationX(const float NewRotX)
+{
+	RelativeRotation.x = NewRotX;
+}
+
+void USceneComponent::SetRelativeRotationY(const float NewRotY)
+{
+	RelativeRotation.y = NewRotY;
+}
+
+void USceneComponent::SetRelativeRotationZ(const float NewRotZ)
+{
+	RelativeRotation.z = NewRotZ;
+}
+
+void USceneComponent::SetRelativeScale3D(const FVector NewScale3D)
+{
+	RelativeScale3D = NewScale3D;
+}
+
+void USceneComponent::SetRelativeScale3DX(const float NewScaleX)
+{
+	RelativeScale3D.x = NewScaleX;
+}
+
+void USceneComponent::SetRelativeScale3DY(const float NewScaleY)
+{
+	RelativeScale3D.y = NewScaleY;
+}
+
+void USceneComponent::SetRelativeScale3DZ(const float NewScaleZ)
+{
+	RelativeScale3D.z = NewScaleZ;
+}
+
+// 월드 좌표에서 옮기고, 이후 Parent와의 상대위치를 이에 따라 업데이트
+void USceneComponent::SetWorldLocation(const FVector NewWorldLocation)
+{
+	if (GetAttachParent() != nullptr)
+	{
+		SetRelativeLocation((GetRelativeLocation4() * GetAttachParent()->GetComponentTransform().Inverse()).xyz());
+	}
+	else
+	{
+		SetRelativeLocation(NewWorldLocation);
+	}
+}
+
+void USceneComponent::SetWorldRotation(const FVector NewWorldRotation)
+{
+	if (GetAttachParent() != nullptr)
+	{
+		SetRelativeRotation((FVector4(GetRelativeRotation(), 0) * GetAttachParent()->GetComponentTransform().Inverse()).xyz());
+	}
+	else
+	{
+		SetRelativeLocation(NewWorldRotation);
+	}
+}
+
+void USceneComponent::SetWorldLocationAndRotation(const FVector NewWorldLocation)
+{
+	FMatrix invTrans = GetAttachParent()->GetComponentTransform().Inverse();
+	SetRelativeLocation((GetRelativeLocation4() * invTrans).xyz());
+	SetRelativeRotation((FVector4(GetRelativeRotation(), 0) * invTrans).xyz());
+}
+
+//void USceneComponent::SetWorldScale3D(const FVector NewWorldScale3D)
+//{
+//
+//}
+
+// const랑 pass by reference(&)
+// 데이터를 효율적으로 전달하면서 호출한 함수가 이 오브젝트의 children을 바꾸지 못하게 함
+// 리턴 값을 다른 변수에 옮기고서("=" operator) 사용
+const TArray<USceneComponent*>& USceneComponent::GetAttachChildren() const
+{
+	return AttachChildern;
+}
+
+void USceneComponent::GetChildrenComponents(TArray<USceneComponent*>& Children) const
+{
+	Children.clear();
+	for (auto& child : Children)
+	{
+		TArray<USceneComponent*> childComponents;
+		child->GetChildrenComponents(childComponents);
+		Children.insert(Children.end(), childComponents.begin(), childComponents.end());
+	}
+}
+
+USceneComponent* USceneComponent::GetAttachParent() const
+{
+	return AttachParent;
+}
+
+void USceneComponent::GetParentComponents(TArray<USceneComponent*>& Parents) const
+{
+	Parents.clear();
+
+	// 언리얼 소스코드 /Engine/Source/Runtime/Engine/Classes/Components/SceneComponent.h 참고
+	USceneComponent* ParentIterator = GetAttachParent();
+	while (ParentIterator != nullptr)
+	{
+		Parents.push_back(ParentIterator);
+		ParentIterator = ParentIterator->GetAttachParent();
+	}
+}
+
+// pass by reference로 메모리 절약
+void USceneComponent::SetupAttachment(TArray<USceneComponent*>& Children)
+{
+	AttachChildern.clear();
+
+	for (auto& child : Children)
+	{
+		if (!child->AttachToComponent(this))
+		{
+			UE_LOG(L"USceneComponent::SetupAttachment::Children으로 지정하는데 실패했습니다.");
+		}
+	}
+}
+
+//void USceneComponent::AttachChildren(USceneComponent* Child)
+//{
+//	if (Child == this)
+//	{
+//		UE_LOG(L"자기 자신을 Children으로 할 수 없습니다.");
+//		return;
+//	}
+//	TArray<USceneComponent*> parents;
+//	GetParentComponents(parents);
+//	for (auto& parent : parents)
+//	{
+//		if (this == parent->GetAttachParent()) {
+//			UE_LOG(L"Ancestor를 Children으로 할 수 없습니다.");
+//			return;
+//		}
+//	}
+//	AttachChildern.push_back(Child);
+//}
+
+bool USceneComponent::AttachToComponent(USceneComponent* Parent)
+{
+	if (Parent == this)
+	{
+		UE_LOG(L"USceneComponent::AttachToComponent::자기 자신을 Parent로 할 수 없습니다.");
+		return false;
+	}
+
+	// parent로 하려는 컴포넌트의 조상에 *this가 있는지 확인
+	TArray<USceneComponent*> parentsOfInput;
+	Parent->GetParentComponents(parentsOfInput);
+	if (parentsOfInput.end() != std::find(parentsOfInput.begin(), parentsOfInput.end(), this))
+	{
+		UE_LOG(L"USceneComponent::AttachToComponent::Descendent를 Parent로 할 수 없습니다.");
+		return false;
+	}
+
+	Parent->AttachChildern.push_back(Parent);
+	AttachParent = Parent;
+	return true;
+
+	//TArray<USceneComponent*> children;
+	//GetChildrenComponents(children);
+	//if (children.end() != std::find(children.begin(), children.end(), this))
+	//{
+
+	//}
+	//return true;
+}
+
+void USceneComponent::PrintLoc(std::wstring msg) const
+{
+	FVector loc = GetRelativeLocation();
+	UE_LOG((std::wstring(L"\n") + msg + std::wstring(L"*************************\n")).c_str());
+	UE_LOG((std::wstring(L"Relative Location\nx :") + std::to_wstring(loc.x) + std::wstring(L" y :")
+		+ std::to_wstring(loc.y) + std::wstring(L" z :") + std::to_wstring(loc.z) + std::wstring(L"\n")).c_str());
+	loc = GetComponentLocation();
+	UE_LOG((std::wstring(L"Component Location\nx :") + std::to_wstring(loc.x) + std::wstring(L" y :")
+		+ std::to_wstring(loc.y) + std::wstring(L" z :") + std::to_wstring(loc.z) + std::wstring(L"\n")).c_str());
+}
+
--- Week01+_KMG/Framework/Core/USceneComponent.h
+++ Week01+_KMG/Framework/Core/USceneComponent.h
@@ -2,10 +2,69 @@
 #include "Framework/Core/UActorComponent.h"
 class USceneComponent :public UActorComponent {
 public:
+	USceneComponent() {}
+	virtual void Update();
+	//virtual void Render();
+public:
 	FVector RelativeLocation = FVector::Zero;
 	FVector RelativeRotation = FVector::Zero;
 	FVector RelativeScale3D = FVector::One;
 
-	FMatrix Transformation();
+	//FMatrix Transformation();
+
+public:
+	FVector GetRelativeLocation() const;
+	FVector4 GetRelativeLocation4() const;
+	FVector GetRelativeRotation() const;
+	FVector GetRelativeScale3D() const;
+	FMatrix GetRelativeTransform() const;
+
+	FVector GetComponentLocation() const;
+	//FVector GetComponentRotation() const;
+	//FVector GetComponentScale() const;
+	FMatrix GetComponentTransform() const;
+	//FVector GetComponentVelocity() const;
+
+	void SetRelativeLocation(const FVector NewLocation);
+	void SetRelativeLocationX(const float NewLocX);
+	void SetRelativeLocationY(const float NewLocY);
+	void SetRelativeLocationZ(const float NewLocZ);
+	void SetRelativeRotation(const FVector NewRotation);
+	void SetRelativeRotationX(const float NewRotX);
+	void SetRelativeRotationY(const float NewRotY);
+	void SetRelativeRotationZ(const float NewRotZ);
+	void SetRelativeScale3D(const FVector NewScale3D);
+	void SetRelativeScale3DX(const float NewScaleX);
+	void SetRelativeScale3DY(const float NewScaleY);
+	void SetRelativeScale3DZ(const float NewScaleZ);
+	//void SetComponentVelocity(const FVector NewVelocity);
+
+	void SetWorldLocation(const FVector NewWorldLocation);
+	void SetWorldRotation(const FVector NewWorldRotation);
+	void SetWorldLocationAndRotation(const FVector NewWorldLocation);
+	//void SetWorldScale3D(const FVector NewWorldScale3D);
+	//void SetWorldVelocity(const FVector NewWorldVelocity);
+
+	//void UpdateComponentVelocity();
+
+// 상속 관련
+protected:
+	USceneComponent* AttachParent = nullptr;
+	TArray<USceneComponent*> AttachChildern;
+
+public:
+	const TArray<USceneComponent*>& GetAttachChildren() const; // 직접적으로 붙은 children만 반환
+	void GetChildrenComponents(TArray<USceneComponent*>& Children) const; // 재귀로 아래의 모든 children 반환
+	USceneComponent* GetAttachParent() const;
+	void GetParentComponents(TArray<USceneComponent*>& Parents) const; // 재귀로 root까지 반환
+
+	void SetupAttachment(TArray<USceneComponent*>& Children); // 자식 설정 실패 가능
+
+	// 부모자식 관계를 실제로 정하는 이 함수 뿐임
+	bool AttachToComponent(USceneComponent* Parent); // 부모 설정 실패했을땐 false
+
+// 디버그
+public:
+	void PrintLoc(std::wstring msg) const;
 };
 
