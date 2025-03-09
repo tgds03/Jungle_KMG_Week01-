17624033e01f14b94d8b2543f29a1d2b280e57b2
jiwon1304 <jiwon1304@gmail.com>
2025-03-09 17:36:32 +0900

    Intersect 추가

--- Week01+_KMG/Framework/Core/UCubeComponent.cpp
+++ Week01+_KMG/Framework/Core/UCubeComponent.cpp
@@ -3,41 +3,41 @@
 #include "./Framework/Core/CRenderer.h"
 
 UCubeComponent::UCubeComponent() {
-	vertices =
-	{
-		// Front Face (¾Õ¸é)
-		{ -0.5f, -0.5f, 0.5f, 1.0f, 0.0f, 0.0f, 1.0f },
-		{ -0.5f,  0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 1.0f },
-		{ 0.5f, -0.5f,  0.5f, 0.0f, 0.0f, 1.0f, 1.0f },
-		{ 0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 0.0f, 1.0f },
+	//vertices =
+	//{
+	//	// Front Face (¾Õ¸é)
+	//	{ -0.5f, -0.5f, 0.5f, 1.0f, 0.0f, 0.0f, 1.0f },
+	//	{ -0.5f,  0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 1.0f },
+	//	{ 0.5f, -0.5f,  0.5f, 0.0f, 0.0f, 1.0f, 1.0f },
+	//	{ 0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 0.0f, 1.0f },
 
-		// Back Face (µÞ¸é)
-		{ -0.5f, -0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 1.0f },
-		{ 0.5f, -0.5f,  -0.5f, 1.0f, 0.0f, 1.0f, 1.0f },
-		{ -0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f },
-		{ 0.5f,  0.5f,  -0.5f, 0.0f, 0.0f, 0.0f, 1.0f }
-	};
+	//	// Back Face (µÞ¸é)
+	//	{ -0.5f, -0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 1.0f },
+	//	{ 0.5f, -0.5f,  -0.5f, 1.0f, 0.0f, 1.0f, 1.0f },
+	//	{ -0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f },
+	//	{ 0.5f,  0.5f,  -0.5f, 0.0f, 0.0f, 0.0f, 1.0f }
+	//};
 
-	indices =
-	{
-		// Front Face (¾Õ¸é)
-		0, 1, 2,  1, 3, 2,
+	//indices =
+	//{
+	//	// Front Face (¾Õ¸é)
+	//	0, 1, 2,  1, 3, 2,
 
-		// Back Face (µÞ¸é)
-		4, 5, 6,  5, 7, 6,
+	//	// Back Face (µÞ¸é)
+	//	4, 5, 6,  5, 7, 6,
 
-		// Left Face (¿ÞÂÊ¸é)
-		4, 6, 0,  6, 1, 0,
+	//	// Left Face (¿ÞÂÊ¸é)
+	//	4, 6, 0,  6, 1, 0,
 
-		// Right Face (¿À¸¥ÂÊ¸é)
-		2, 3, 5,  3, 7, 5,
+	//	// Right Face (¿À¸¥ÂÊ¸é)
+	//	2, 3, 5,  3, 7, 5,
 
-		// Top Face (À­¸é)
-		1, 6, 3,  6, 7, 3,
+	//	// Top Face (À­¸é)
+	//	1, 6, 3,  6, 7, 3,
 
-		// Bottom Face (¾Æ·§¸é)
-		0, 2, 4,  2, 5, 4
-	};
+	//	// Bottom Face (¾Æ·§¸é)
+	//	0, 2, 4,  2, 5, 4
+	//};
 
 	vertices = {
 		{1.f, 1.f, -1.f, 1.f, 0.f, 0.f, 1.f},   // Vertex 0.
@@ -94,4 +94,31 @@ UCubeComponent::~UCubeComponent() {
 void UCubeComponent::Update() {
 	//RelativeRotation += FVector(1.f, 1.f, 1.f) * Time::GetDeltaTime();
 	//RelativeLocation.x = sin(Time::GetElapsedTime());
+}
+
+bool UCubeComponent::Intersects(FVector rayOrigin, FVector rayDirection)
+{
+	FVector4 rayOrigin4(rayOrigin, 1.f);
+	rayOrigin4 = rayOrigin4 * GetComponentTransform().Inverse();
+	rayOrigin4 = rayOrigin4 / rayOrigin4.w;
+
+	FVector4 rayDirection4(rayDirection, 0.f);
+	rayDirection4 = rayDirection4 * GetComponentTransform().Inverse();
+
+	float t_min_x = (-1 - rayOrigin4.x) / rayDirection4.x;
+	float t_max_x = (1 - rayOrigin4.x) / rayDirection4.x;
+	if (rayDirection4.x < 0) std::swap(t_min_x, t_max_x);
+
+	float t_min_y = (-1 - rayOrigin4.y) / rayDirection4.y;
+	float t_max_y = (1 - rayOrigin4.y) / rayDirection4.y;
+	if (rayDirection4.y < 0) std::swap(t_min_y, t_max_y);
+
+	float t_min_z = (-1 - rayOrigin4.z) / rayDirection4.z;
+	float t_max_z = (1 - rayOrigin4.z) / rayDirection4.z;
+	if (rayDirection4.z < 0) std::swap(t_min_z, t_max_z);
+	
+	float max_of_t_min = max(max(t_min_x, t_min_y), t_min_z);
+	float min_of_t_max = min(min(t_max_x, t_max_y), t_max_z);
+
+	return (max_of_t_min <= min_of_t_max) && (min_of_t_max >= 0);
 }
